e250ab791d423a66fbf8d77e22dec8522ddaccb8
Leigh Michael Forrest <leigh@catbirdseat.io>
2024-02-26 13:36:39 -0500

    cleaned up pytest files

---
+++ tests/functional/test_pages.py
@@ -0,0 +1,34 @@
+class TestPages:
+    def test_index_page(self, client):
+        response = client.get("/")
+        assert response.status_code == 200
+        assert b"Welcome to Website App" in response.data
+
+
+    def test_about_page(self, client):
+        response = client.get("/about")
+        assert response.status_code == 200
+        assert b"This is the about page of Website App" in response.data
+
+
+    def test_get_write_message(self, client):
+        response = client.get("/message/write")
+        assert response.status_code == 200
+        assert b"Add Message" in response.data
+        assert b"<input" in response.data
+
+
+    def test_post_write_message(self, capsys, client):
+        message = "Hello from Pytest!"
+        response = client.post(
+            "/message/write", data={"message": message}, follow_redirects=True
+        )
+
+        # test the print statement
+        captured = capsys.readouterr()
+        assert f"Message: {message}" in captured.out
+
+        # test the response
+        assert response.status_code == 200
+        assert bytes(message, "utf-8") in response.data
+        assert b"Welcome to Website App" in response.data
