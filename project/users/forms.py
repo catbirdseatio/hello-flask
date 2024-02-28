3a124d0ddf5f29a892e96e2670fe02141438e39a
Leigh Michael Forrest <leigh@catbirdseat.io>
2024-02-28 12:40:02 -0500

    custom validator added to registration form

--- project/users/forms.py
+++ project/users/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, ValidationError
+import sqlalchemy as sa
+from project import db
+from project.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,3 +12,9 @@ class RegistrationForm(FlaskForm):
     ])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=5, max=40)])
     submit = SubmitField("Register")
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError(f"{email.data} already exists.")
--- project/users/routes.py
+++ project/users/routes.py
@@ -12,15 +12,11 @@ def register():
 
     if request.method == "POST":
         if form.validate_on_submit():
-            try:
-                new_user = User(form.email.data, form.password.data)
-                db.session.add(new_user)
-                db.session.commit()
-                flash(f"Thanks for registering, {form.email.data}", "success")
-                return redirect(url_for("pages.index"))
-            except IntegrityError:
-                db.session.rollback()
-                flash(f"{form.email.data} already exists.", "warning")
+            new_user = User(form.email.data, form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            flash(f"Thanks for registering, {form.email.data}", "success")
+            return redirect(url_for("pages.index"))
         else:
             flash("Error in form data.", "danger")
     return render_template("users/registration.html", form=form)
