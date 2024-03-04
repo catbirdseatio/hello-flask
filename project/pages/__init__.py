08510f4c79da78adb6345825992b9dc85226e670
Leigh Michael Forrest <leigh@catbirdseat.io>
2024-03-04 09:18:26 -0500

    password reset functional

--- project/pages/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-pages_blueprint = Blueprint("pages", __name__, template_folder="templates")
-
-from . import routes
--- tests/unit/test_models.py
+++
@@ -1,9 +0,0 @@
-from project.models import Message
-
-
-def test_new_message(message):
-    assert message.message == "Hello Python!"
-
-
-def test___str__(message):
-    assert str(message) == "Message: Hello Python!"
